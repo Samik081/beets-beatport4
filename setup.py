d88f1841fd47b73a4f4583a2b56547bf83c3b9b5
Szymon Tarasiński <szymon.tarasinski@docplanner.com>
2022-09-08 14:26:17 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='beets-beatport4',
-    version='0.1.0',
+    version='0.1.1',
     description='Plugin for beets (http://beets.io) to replace stock beatport plugin which is not yet compatible '
                 'with Beatport API v4.',
     long_description=open('README.rst').read(),
