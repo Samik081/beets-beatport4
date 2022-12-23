4988b33a47c26ae8f8e917ae3073eca83522c3fb
Szymon Tarasiński <szymon.tarasinski@docplanner.com>
2022-12-23 15:06:17 +0100

    Bring up version to 0.2.9

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name='beets-beatport4',
-    version='0.2.8',
+    version='0.2.9',
     description='Plugin for beets (http://beets.io) to replace stock beatport plugin which is not yet compatible '
                 'with Beatport API v4.',
     long_description=open('README.rst').read(),
     author='Szymon "Samik" Tarasinski',
     author_email='st.samik@gmail.com',
     url='https://github.com/Samik081/beets-beatport4',
-    download_url='https://github.com/Samik081/beets-beatport4/releases/download/v0.2.8/beets-beatport4-0.2.8.tar.gz',
+    download_url='https://github.com/Samik081/beets-beatport4/releases/download/v0.2.9/beets-beatport4-0.2.9.tar.gz',
     license='MIT',
     platforms='ALL',
 
