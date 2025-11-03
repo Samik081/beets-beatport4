9f50e9c8dbfd069d9bd2bbfa7c9d1303e0e0d451
Szymon Tarasiński <szymon.tarasinski@docplanner.com>
2025-11-03 10:44:57 +0100

    chore: update setup.py requirements, classifiers, versions and urls

--- setup.py
+++ setup.py
@@ -2,21 +2,21 @@ from setuptools import setup
 
 setup(
     name='beets-beatport4',
-    version='0.3.4',
+    version='0.4.0',
     description='Plugin for beets (http://beets.io) to replace stock beatport plugin which is not yet compatible '
                 'with Beatport API v4.',
     long_description=open('README.rst').read(),
     author='Szymon "Samik" Tarasinski',
     author_email='st.samik@gmail.com',
     url='https://github.com/Samik081/beets-beatport4',
-    download_url='https://github.com/Samik081/beets-beatport4/releases/download/v0.3.3/beets-beatport4-0.3.4.tar.gz',
+    download_url='https://github.com/Samik081/beets-beatport4/releases/download/v0.4.0/beets-beatport4-0.4.0.tar.gz',
     license='MIT',
     platforms='ALL',
 
     packages=['beetsplug'],
 
     install_requires=[
-        'beets>=1.6.0',
+        'beets>=2.5.1',
         'requests',
         'confuse'
     ],
@@ -33,5 +33,8 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
 )
